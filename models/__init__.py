bc98f5bb6404cc11e5550dee55177b5200af63d4
harshavardhan784 <harshavardhan.govind@gmail.com>
2026-04-09 17:45:59 +0530

    torchvision import

--- models/__init__.py
+++ models/__init__.py
@@ -3,18 +3,8 @@
 Import from this package in training/inference scripts to keep paths stable.
 """
 
-from models.layers import CustomDropout
-from models.localization import VGG11Localizer
-from models.classification import VGG11Classifier
-from models.segmentation import VGG11UNet
 from models.vgg11 import VGG11Encoder
-from models.multitask import MultiTaskPerceptionModel
 
 __all__ = [
-    "CustomDropout",
-    "VGG11Classifier",
     "VGG11Encoder",
-    "VGG11Localizer",
-    "VGG11UNet",
-    "MultiTaskPerceptionModel",
 ]
